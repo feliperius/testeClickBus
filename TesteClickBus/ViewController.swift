f012291fa258d7d27eb8d1bae3ddd27ac332fb9c
felipe <felipe.perius@gmail.com>
2018-10-28 17:26:20 -0300

    initial commit

---
+++ TesteClickBus/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TesteClickBus
+//
+//  Created by Felipe perius on 28/10/2018.
+//  Copyright © 2018 Felipe perius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
